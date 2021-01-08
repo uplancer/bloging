966a527546fc23adfd9d43c49455f60fdc746d1b
Abdu Masoudi <uplancer@protonmail.com>
2021-01-08 14:04:37 +0100

    feat: Add get single post method

--- models/Post.php
+++ models/Post.php
@@ -43,6 +43,40 @@
       $stmt->execute();
 
       return $stmt;
+    }
 
+    // Get Single Post
+    public function readSingle() {
+      $sql = 'SELECT c.name as category_name,
+      p.id,
+      p.category_id,
+      p.title,
+      p.body,
+      p.author,
+      p.created_at
+      FROM ' . $this->table . ' p
+      LEFT JOIN
+        categories c ON p.category_id = c.id
+      WHERE
+        p.id = ?
+      LIMIT 0,1';
+      
+      // Prepare statement
+      $stmt = $this->conn->prepare($sql);
+
+      // Bind ID
+      $stmt->bindParam(1, $this->id);
+
+      // Execute Query
+      $stmt->execute();
+
+      $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+      // Set properties
+      $this->title = $row['title'];
+      $this->body = $row['body'];
+      $this->author = $row['author'];
+      $this->category_id = $row['category_id'];
+      $this->category_name = $row['category_name'];
     }
   }
